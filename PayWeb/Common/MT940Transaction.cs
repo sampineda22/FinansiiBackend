b885233a4f321c1a39c12e1b048839127a453845
LeonelHerculesIntermoda <82479435+LeonelHerculesIntermoda@users.noreply.github.com>
2024-04-18 08:40:39 -0600

    Cambios en logica de lectura de archivo MT940

---
+++ PayWeb/Common/MT940Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Common
+{
+    public class MT940Transaction
+    {
+        public string Account { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Type { get; set; }
+        public string Reference { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"Account:{Account}, Date: {Date}, Monto: {Amount}, Type: {Type}, Reference: {Reference}, Descripcion: {Description}";
+        }
+    }
+}
